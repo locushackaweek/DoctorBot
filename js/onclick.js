086c0507dddf73751b7d3db480769c35a11db87d
Zero <minatouzumakin001@gmail.com>
2018-01-24 06:51:19 +0545

    changes a lot of things, added labels

--- js/onclick.js
+++ js/onclick.js
@@ -1,6 +1,22 @@
+//Global variables
+var submit;
+var selection;
+var questions;
 
 
+window.onload = prepare();
 
-$('#dc-submit-button').click(function() {
-    $('#dc-main-display-area').html('Other questions...');
-});
+function prepare()
+{
+    submit = document.getElementById("dc-submit");
+    selection = document.getElementById("dc-selection");
+    questions = document.getElementById("dc-questions");
+
+    submit.addEventListener("click", askQuestions, false);
+}
+
+function askQuestions()
+{
+    selection.style.display = "none";
+    questions.style.display = "block";
+}
